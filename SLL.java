ff2dd480fcf7851cf5867afc8d55f5273dc5939a
Anastasija Radenkovic <aradenkovic@smith.edu>
2026-02-27 22:25:59 -0500

    ..

--- SLL.java
+++ SLL.java
@@ -2,155 +2,121 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- * A singly linked list implementation.
+ * A generic singly linked list implementation.
  *
- * @param <T> element type
+ * @param <T> the type of elements stored in the list
  */
-public class SLL<T> implements ListADT<T> {
+public class SLL<T> implements Iterable<T> {
 
-    /** Node class */
-    private static class Node<T> {
-        T data;
-        Node<T> next;
-
-        Node(T data) {
-            this.data = data;
-        }
-    }
-
-    private Node<T> head;
+    private NodeSL<T> head;
     private int size;
 
-    @Override
-    public int size() {
-        return size;
+    /**
+     * Constructs an empty singly linked list.
+     */
+    public SLL() {
+        head = null;
+        size = 0;
     }
 
-    private void checkIndexInclusive(int index) {
-        if (index < 0 || index > size)
-            throw new IndexOutOfBoundsException();
+    /**
+     * Returns the head node of the list.
+     *
+     * @return the first node in the list, or null if empty
+     */
+    public NodeSL<T> getHead() {
+        return head;
     }
 
-    private void checkIndexExclusive(int index) {
-        if (index < 0 || index >= size)
-            throw new IndexOutOfBoundsException();
-    }
+    /**
+     * Adds an element to the end of the list.
+     *
+     * @param data the element to add
+     */
+    public void addLast(T data) {
+        NodeSL<T> newNode = new NodeSL<>(data, null);
 
-    private Node<T> nodeAt(int index) {
-        Node<T> curr = head;
-        for (int i = 0; i < index; i++)
-            curr = curr.next;
-        return curr;
-    }
-
-    @Override
-    public T get(int index) {
-        checkIndexExclusive(index);
-        return nodeAt(index).data;
-    }
-
-    @Override
-    public void add(int index, T element) {
-        checkIndexInclusive(index);
-
-        if (index == 0) {
-            Node<T> n = new Node<>(element);
-            n.next = head;
-            head = n;
+        if (head == null) {
+            head = newNode;
         } else {
-            Node<T> prev = nodeAt(index - 1);
-            Node<T> n = new Node<>(element);
-            n.next = prev.next;
-            prev.next = n;
+            NodeSL<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
         }
         size++;
     }
 
-    @Override
-    public T remove(int index) {
-        checkIndexExclusive(index);
-
-        Node<T> removed;
-        if (index == 0) {
-            removed = head;
-            head = head.next;
-        } else {
-            Node<T> prev = nodeAt(index - 1);
-            removed = prev.next;
-            prev.next = removed.next;
-        }
-        size--;
-        return removed.data;
-    }
-
-    @Override
-    public SLL<T> splitCopy(int index) {
-        checkIndexInclusive(index);
-
-        SLL<T> result = new SLL<>();
-        if (index == size)
-            return result;
-
-        Node<T> curr = nodeAt(index);
-        while (curr != null) {
-            result.add(result.size, curr.data);
-            curr = curr.next;
-        }
-        return result;
-    }
-
-    @Override
-    public SLL<T> splitTransfer(int index) {
-        checkIndexInclusive(index);
-
-        SLL<T> result = new SLL<>();
-
-        if (index == 0) {
-            result.head = head;
-            result.size = size;
-            head = null;
-            size = 0;
-            return result;
+    /**
+     * Inserts a new element after a given node.
+     *
+     * @param node the node after which the new element is inserted
+     * @param data the element to insert
+     */
+    public void addAfter(NodeSL<T> node, T data) {
+        if (node == null) {
+            throw new IllegalArgumentException("Node cannot be null");
         }
 
-        if (index == size)
-            return result;
-
-        Node<T> prev = nodeAt(index - 1);
-        result.head = prev.next;
-        result.size = size - index;
-
-        prev.next = null;
-        size = index;
-
-        return result;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new SLLIterator();
+        NodeSL<T> newNode = new NodeSL<>(data, node.getNext());
+        node.setNext(newNode);
+        size++;
     }
 
     /**
-     * Iterator for SLL walking node-by-node.
+     * Removes the node immediately following the given node.
+     *
+     * @param node the node before the one to remove
+     * @return the data of the removed node
      */
-    private class SLLIterator implements Iterator<T> {
-
-        private Node<T> current = head;
-
-        @Override
-        public boolean hasNext() {
-            return current != null;
+    public T removeAfter(NodeSL<T> node) {
+        if (node == null || node.getNext() == null) {
+            throw new IllegalArgumentException("No node exists after the given node");
         }
 
-        @Override
-        public T next() {
-            if (!hasNext())
-                throw new NoSuchElementException();
+        NodeSL<T> toRemove = node.getNext();
+        node.setNext(toRemove.getNext());
+        size--;
 
-            T data = current.data;
-            current = current.next;
-            return data;
-        }
+        return toRemove.getData();
+    }
+
+    /**
+     * Returns the number of elements in the list.
+     *
+     * @return the size of the list
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns an iterator over the elements in this list.
+     *
+     * @return an iterator
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private NodeSL<T> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                T data = current.getData();
+                current = current.getNext();
+                return data;
+            }
+        };
     }
 }
